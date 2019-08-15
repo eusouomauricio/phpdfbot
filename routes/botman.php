620016bec98eabd2377a88a974b0da0a895fc4ad
@se45ky <marcelo2208@gmail.com>
2019-08-15 23:50:40 +0200

    Configure Botman to use Dialogflow in @DevPhpDfBot

--- routes/botman.php
+++ routes/botman.php
@@ -1,27 +1,28 @@
 <?php
 use App\Http\Controllers\BotManController;
 use BotMan\BotMan\Middleware\ApiAi;
+use BotMan\BotMan\BotMan;
 
 $botman = resolve('botman');
 
-$dialogflow = ApiAi::create('b0b113319a1e409e86d931cecc67444c')->listenForAction();
+$dialogflow = ApiAi::create(env('DIALOGFLOW_TOKEN'))->listenForAction();
+$botman->middleware->received($dialogflow);
+$botman->hears('.*', function (BotMan $bot) {
+
+    $extras = $bot->getMessage()->getExtras();
+    $apiReply = $extras['apiReply'];
+    $apiAction = $extras['apiAction'];
+    $apiIntent = $extras['apiIntent'];
+
+    $bot->reply($apiReply);
+//    $bot->reply("this is my reply: " . json_encode($extras));
+
+})->middleware($dialogflow);
 
 // Apply global "received" middleware
-$botman->middleware->received($dialogflow);
-
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
-});
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
-
-//// Apply matching middleware per hears command
-//$botman->hears('my_api_action', function (\BotMan\BotMan\BotMan $bot) {
-//    // The incoming message matched the "my_api_action" on Dialogflow
-//    // Retrieve Dialogflow information:
-//    $extras = $bot->getMessage()->getExtras();
-//    $apiReply = $extras['apiReply'];
-//    $apiAction = $extras['apiAction'];
-//    $apiIntent = $extras['apiIntent'];
+//$botman->middleware->received($dialogflow);
 //
-//    $bot->reply("this is my reply");
-//})->middleware($dialogflow);
+//$botman->hears('Hi', function ($bot) {
+//    $bot->reply('Hello!');
+//});
+//$botman->hears('Start conversation', BotManController::class.'@startConversation');
